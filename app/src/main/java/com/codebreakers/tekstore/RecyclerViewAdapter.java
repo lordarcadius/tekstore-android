788d919e28730943a533ad8a6aa541a7bae515e2
Vipul Jha <vipuljha08@gmail.com>
2020-04-14 17:00:30 +0530

    app: Initial RecyclerView code for Products page
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

---
+++ app/src/main/java/com/codebreakers/tekstore/RecyclerViewAdapter.java
@@ -0,0 +1,35 @@
+package com.codebreakers.tekstore;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
+
+    Context mContext;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class  MyViewHolder extends  RecyclerView.ViewHolder{
+        public MyViewHolder(View itemView){
+            super(itemView);
+        }
+    }
+}
